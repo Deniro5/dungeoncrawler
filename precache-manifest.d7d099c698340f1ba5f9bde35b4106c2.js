cad7eadb9a488df72cc7b45591c4d73f11e55b90
USER <user@USERs-MacBook-Pro.local>
2020-04-25 18:04:06 -0400

    Updates

---
+++ precache-manifest.d7d099c698340f1ba5f9bde35b4106c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3960d14ffbac624967b",
+    "url": "/dungeoncrawler/./static/css/main.73c88fe8.chunk.css"
+  },
+  {
+    "revision": "c3960d14ffbac624967b",
+    "url": "/dungeoncrawler/./static/js/main.c3960d14.chunk.js"
+  },
+  {
+    "revision": "c1df04674da97935aa86",
+    "url": "/dungeoncrawler/./static/js/1.c1df0467.chunk.js"
+  },
+  {
+    "revision": "30e69c3a0ba2a8f041b0",
+    "url": "/dungeoncrawler/./static/js/runtime~main.30e69c3a.js"
+  },
+  {
+    "revision": "4a75db6d517828e09fa73f41f9a7d5dd",
+    "url": "/dungeoncrawler/./index.html"
+  }
+];
